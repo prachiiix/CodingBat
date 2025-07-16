9c292d92e1d24fd1613d58783cc9fad46119be0b
Prachi Chougule <prachichougule.009@gmail.com>
2025-07-17 03:09:20 +0530

    Create strCopies.java

---
+++ Recursion/strCopies.java
@@ -0,0 +1,21 @@
+
+// Given a string and a non-empty substring sub, compute recursively if at least n copies of sub appear in the string somewhere, possibly with overlapping. N will be non-negative.
+
+
+// strCopies("catcowcat", "cat", 2) → true
+// strCopies("catcowcat", "cow", 2) → false
+// strCopies("catcowcat", "cow", 1) → true
+public boolean strCopies(String str, String sub, int n) {
+  if(n == 0){
+    return true;
+  }
+  if(str.length() < sub.length()){
+    return false;
+  }
+  else if(str.startsWith(sub)){
+    return strCopies(str.substring(1), sub, n-1);
+  }
+  else{
+    return strCopies(str.substring(1), sub, n);
+  }
+}
